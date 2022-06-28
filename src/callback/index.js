b6146f9e6b9ded08a18dd95e6438782cc82e47f1
Cristian Rojas <cristian.rojas@gco.com.co>
2022-06-28 14:13:33 -0500

    callback

---
+++ src/callback/index.js
@@ -0,0 +1,21 @@
+ function sum(num1,num2){
+    return num1 + num2
+ }
+
+ function calc(num1, num2, callback){
+    return callback(num1,num2)
+ }
+
+ console.log( calc(2,2,sum))
+
+
+
+ setTimeout(() => {
+    console.log('hola JS ')
+ }, 2000);
+
+ function grettin(name){
+    console.log(`Hola ${name}`)
+ }
+
+setTimeout(grettin, 2000,'Cristian');
